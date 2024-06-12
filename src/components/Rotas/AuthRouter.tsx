29de6b4fada1d327364f7cf3f9d382e8daaf2d0d
Lucas Lopes <lucas.lopes.pb@compasso.com.br>
2024-06-12 10:35:59 -0300

    feat: routes auth

---
+++ src/components/Rotas/AuthRouter.tsx
@@ -0,0 +1,24 @@
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Navigate } from "react-router-dom";
+import { auth } from "./seu-arquivo-de-configuracao-do-firebase"; // Esperando Talita me ajudar
+
+type RouterProps = {
+  children: React.ReactNode;
+  isPrivate?: boolean;
+};
+
+export function AuthRouter({ children, isPrivate = true }: RouterProps) {
+  const [user, loading] = useAuthState(auth);
+
+  if (loading) return null;
+
+  if (isPrivate && !user) {
+    return <Navigate to="/login" />;
+  } else if (!isPrivate && user) {
+    return <Navigate to="/" />;
+  } else if (!isPrivate && !user) {
+    return <Navigate to="/" />;
+  }
+
+  return <>{children}</>;
+}
