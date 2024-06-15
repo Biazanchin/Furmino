ecd8ac3bc4b34028ddc9abf0aed39add726f20d7
Biazanchin <beatrizzanquimmessias@gmail.com>
2024-06-15 15:55:31 -0300

    chore: change the show more logic

--- src/components/Home/Products.tsx
+++ src/components/Home/Products.tsx
@@ -4,12 +4,12 @@ import CardItem from "./Cards";
 
 const OurProducts = () => {
   const [products, setProducts] = useState<Products[]>([]);
-  const [visibleProducts, setVisibleProducts] = useState<number>(8);
+  const [visibleProducts] = useState<number>(8);
 
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const response = await fetch("https://run.mocky.io/v3/b8c85b8e-c246-4926-b643-226ead116300");
+        const response = await fetch("https://run.mocky.io/v3/978b1590-629d-4bf8-9768-31d8b292b888");
         const data = await response.json();
         setProducts(data.products);
       } catch (error) {
@@ -18,10 +18,6 @@ const OurProducts = () => {
     };
     fetchProducts();
   }, []);
-
-  const handleShowMore = () => {
-    setVisibleProducts((prev) => prev + 8);
-  };
 
   return (
     <section className="py-20 max-sm:px-4">
@@ -34,16 +30,14 @@ const OurProducts = () => {
             <CardItem key={product.sku} product={product} />
           ))}
         </div>
-        {visibleProducts < products.length && (
           <div className="text-center mt-8">
-            <button 
-              onClick={handleShowMore} 
+            <a 
+             href="/shop"
               className="border border-primary text-primary px-10 py-2"
             >
               Show More
-            </button>
+            </a>
           </div>
-        )}
       </div>
     </section>
   );
