6000d43eee70958740156100e694032e7b310be0
Biazanchin <beatrizzanquimmessias@gmail.com>
2024-06-18 18:54:00 -0300

    feat: create CartPage

--- src/components/Cart/CartPage.tsx
+++ src/components/Cart/CartPage.tsx
@@ -19,60 +19,86 @@ const CartPage = () => {
   const total = products.reduce((sum, { product, quantity }) => sum + product.price * quantity, 0);
 
   return (
-    <div className="container mx-auto mt-10 p-6">
+    <div className="container mx-auto font-poppins p-10 lg:px-20 pb-20">
       {products.length > 0 ? (
-        <div className="flex flex-col md:flex-row">
-          <div className="flex-1 mb-6 md:mb-0">
-            <div className="bg-filter p-4 mb-4">
-              <div className="flex justify-between font-semibold">
-                <span className="w-1/3">Product</span>
-                <span className="w-1/6 text-center">Price</span>
-                <span className="w-1/6 text-center">Quantity</span>
-                <span className="w-1/6 text-right">Subtotal</span>
+        <div className="lg:flex lg:flex-row lg:space-x-6">
+          <div className="flex-1 mb-6">
+            <div className="bg-white p-4 mb-4 md:bg-filter">
+              <div className="md:flex justify-between font-semibold hidden">
+                <span className='lg:ml-32'>Product</span>
+                <span>Price</span>
+                <span>Quantity</span>
+                <span className='lg:mr-32'>Subtotal</span>
               </div>
             </div>
-            {products.map(({ product, quantity }) => (
-              <div key={product.sku} className="flex items-center mb-4 pb-4">
-                <div className="w-1/3 flex items-center">
-                  <img src={product.imgUrl[0]} alt={product.name} className="w-20 h-20 rounded-lg" />
-                  <div className="ml-4">
-                    <h3 className="font-semibold">{product.name}</h3>
+            <div className="flex-col sm:flex-row">
+              {products.map(({ product, quantity }) => (
+                <div key={product.sku} className="flex flex-col sm:flex-row items-center mb-4 pb-4 sm:items-center">
+                  <div className="w-full sm:w-1/3 flex flex-col sm:flex-row items-center">
+                    <img src={product.imgUrl[0]} alt={product.name} className="w-20 h-20 rounded-lg" />
+                    <div className="sm:ml-4 mt-2 sm:mt-0">
+                      <h3 className="font-semibold text-gray text-center sm:text-left lg:ml-14">{product.name}</h3>
+                    </div>
                   </div>
+                  <p className="w-full sm:w-1/6 text-center text-gray-600 mt-2 sm:mt-0 sm:block hidden text-gray">Rs. {product.price.toLocaleString()}</p>
+                  <div className="flex justify-center items-center py-2 px-2 border border-light-gray rounded-lg mt-2 sm:mt-0 sm:ml-8">
+                    <button
+                      onClick={() => handleQuantityChange(product, quantity - 1)}
+                      className="mr-4"
+                      disabled={quantity <= 1}
+                    >
+                      -
+                    </button>
+                    <span className="mx-2">{quantity}</span>
+                    <button
+                      onClick={() => handleQuantityChange(product, quantity + 1)}
+                      className="ml-4"
+                    >
+                      +
+                    </button>
+                  </div>
+                  <p className="w-full sm:w-1/6 text-center sm:text-right mt-2 sm:mt-0 sm:ml-10">Rs. {(product.price * quantity).toLocaleString()}</p>
+                  <button onClick={() => handleRemoveProduct(product.sku)} className="ml-4 mt-2 sm:mt-0 text-red-500">
+                    <img className="lg:ml-10 mr-2" src="https://desafio3furniro.s3.us-east-2.amazonaws.com/cart/lixeira.png" alt="lixeira" />
+                  </button>
                 </div>
-                <p className="w-1/6 text-center">Rs. {product.price.toLocaleString()}</p>
-                <div className="w-1/6 flex justify-center items-center">
-                  <button
-                    onClick={() => handleQuantityChange(product, quantity - 1)}
-                    className="px-2 py-1 border rounded-md"
-                    disabled={quantity <= 1}
-                  >-</button>
-                  <span className="mx-2">{quantity}</span>
-                  <button
-                    onClick={() => handleQuantityChange(product, quantity + 1)}
-                    className="px-2 py-1 border rounded-md"
-                  >+</button>
-                </div>
-                <p className="w-1/6 text-right">Rs. {(product.price * quantity).toLocaleString()}</p>
-                <button onClick={() => handleRemoveProduct(product.sku)} className="ml-4 text-red-500">
-                  <img src='https://desafio3furniro.s3.us-east-2.amazonaws.com/cart/lixeira.png' alt='lixeira'/>
-                </button>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
-          <div className="w-full md:w-1/3 bg-filter p-6">
-            <h2 className="text-xl font-semibold mb-4">Cart Totals</h2>
-            <div className="flex justify-between mb-2">
-              <span>Subtotal:</span>
+          <div className="hidden lg:block w-1/3 bg-filter p-2 pl-12 pr-12 ml-6 h-[300px] sm:h-80">
+            <h2 className="text-2xl font-semibold mb-12 text-center">Cart Totals</h2>
+            <div className="flex justify-between mb-6">
+              <span className='font-semibold'>Subtotal:</span>
               <span className='text-gray'>Rs. {total.toLocaleString()}</span>
             </div>
             <div className="flex justify-between font-semibold text-lg">
               <span>Total:</span>
               <span className='text-primary'>Rs. {total.toLocaleString()}</span>
             </div>
-            <Link
-              to="/checkout"
-              className="block text-center border border-black rounded-lg mt-4 hover:font-bold"
-            >Check Out</Link>
+            <div className='lg:px-20'>
+              <Link
+                to="/checkout"
+                className="block text-center border border-black rounded-lg mt-12 py-2 lg:py-3 hover:font-bold"
+              >Check Out</Link>
+            </div>
+          </div>
+          {/* Responsividade */}
+          <div className="lg:hidden w-full bg-filter p-2 pl-4 pr-4 mt-6">
+            <h2 className="text-xl font-semibold mb-4 text-center">Cart Totals</h2>
+            <div className="flex justify-between mb-4">
+              <span className='font-semibold'>Subtotal:</span>
+              <span className='text-gray'>Rs. {total.toLocaleString()}</span>
+            </div>
+            <div className="flex justify-between font-semibold text-lg">
+              <span>Total:</span>
+              <span className='text-primary'>Rs. {total.toLocaleString()}</span>
+            </div>
+            <div className='px-20 md:px-60'>
+              <Link
+                to="/checkout"
+                className="block text-center border border-black rounded-lg py-2 hover:font-bold"
+              >Check Out</Link>
+            </div>
           </div>
         </div>
       ) : (
