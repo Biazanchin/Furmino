6f0b03ebce072feb0b08fd285d8b7121c3eb1fbd
Beatriz Zanchin <betrizmesias@gmail.com>
2024-06-20 01:20:38 -0300

    feat: improved responsiveness

--- src/components/Beneficios/Benefits.tsx
+++ src/components/Beneficios/Benefits.tsx
@@ -1,48 +1,64 @@
 function Benefits() {
   return (
     <div className="mb-2">
-        <div className="bg-light-pink p-8">
+      <div className="bg-light-pink p-8">
         <div className="max-w-screen-xl mx-auto space-y-6">
-            <div className="flex justify-between">
+          <div className="flex justify-between">
             <div className="flex items-center space-x-4">
-                <div>
-                <img src="https://desafio3furniro.s3.us-east-2.amazonaws.com/feature/trophy+1.png" alt="High Quality" />
-                </div>
-                <div>
+              <div>
+                <img
+                  src="https://desafio3furniro.s3.us-east-2.amazonaws.com/feature/trophy+1.png"
+                  alt="High Quality"
+                  className="lg:flex sm:hidden"
+                />
+              </div>
+              <div>
                 <h3 className="text-lg font-semibold">High Quality</h3>
                 <p>Crafted from top materials</p>
-                </div>
+              </div>
             </div>
             <div className="flex items-center space-x-4">
-                <div>
-                <img src="https://desafio3furniro.s3.us-east-2.amazonaws.com/feature/guarantee.png" alt="Warranty Protection" />
-                </div>
-                <div>
+              <div>
+                <img
+                  src="https://desafio3furniro.s3.us-east-2.amazonaws.com/feature/guarantee.png"
+                  alt="Warranty Protection"
+                  className="lg:flex sm:hidden"
+                />
+              </div>
+              <div>
                 <h3 className="text-lg font-semibold">Warranty Protection</h3>
                 <p>Over 2 years</p>
-                </div>
+              </div>
             </div>
             <div className="flex items-center space-x-4">
-                <div>
-                <img src="https://desafio3furniro.s3.us-east-2.amazonaws.com/feature/shipping.png" alt="Free Shipping" />
-                </div>
-                <div>
+              <div>
+                <img
+                  src="https://desafio3furniro.s3.us-east-2.amazonaws.com/feature/shipping.png"
+                  alt="Free Shipping"
+                  className="lg:flex sm:hidden"
+                />
+              </div>
+              <div>
                 <h3 className="text-lg font-semibold">Free Shipping</h3>
                 <p>Order over $50</p>
-                </div>
+              </div>
             </div>
             <div className="flex items-center space-x-4">
-                <div>
-                <img src="https://desafio3furniro.s3.us-east-2.amazonaws.com/feature/customer-support.png" alt="24/7 Support" />
-                </div>
-                <div>
+              <div>
+                <img
+                  src="https://desafio3furniro.s3.us-east-2.amazonaws.com/feature/customer-support.png"
+                  alt="24/7 Support"
+                  className="lg:flex sm:hidden"
+                />
+              </div>
+              <div>
                 <h3 className="text-lg font-semibold">24/7 Support</h3>
                 <p>Dedicated support</p>
-                </div>
+              </div>
             </div>
-            </div>
+          </div>
         </div>
-        </div>
+      </div>
     </div>
   );
 }
