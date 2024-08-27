0515c37b1585b6db5b41ec6be99d462936e81030
Biazanchin <beatrizzanquimmessias@gmail.com>
2024-08-27 16:57:00 -0300

    fix: improve filter logic and visibility

--- src/components/Shop/CardShop.tsx
+++ src/components/Shop/CardShop.tsx
@@ -8,17 +8,16 @@ function CardShop() {
   const [filteredProducts, setFilteredProducts] = useState<Products[]>([]);
   const [, setSelectedProduct] = useState<string | null>(null);
   const [currentPage, setCurrentPage] = useState(1);
+  const [filtersVisible] = useState(false);
   const itemsPerPage = 16;
 
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const response = await fetch(
-          "https://run.mocky.io/v3/41508ff4-254e-4a3d-8da8-47aea9b0d5d3"
-        );
+        const response = await fetch("http://localhost:5000/products");
         const data = await response.json();
-        setAllProducts(data.products);
-        setFilteredProducts(data.products);
+        setAllProducts(data);
+        setFilteredProducts(data);
       } catch (error) {
         console.log("Error fetching products: ", error);
       }
@@ -41,17 +40,24 @@ function CardShop() {
       filtered = filtered.filter((product) => product.category === category);
     }
 
-    if (!category && sortBy === "alphabetical") {
+    if (sortBy === "alphabetical") {
       filtered.sort((a, b) => a.name.localeCompare(b.name));
-    } else if (!category && sortBy === "price") {
+    } else if (sortBy === "price") {
       filtered.sort((a, b) => a.price - b.price);
-    } else if (isNew) {
+    }
+
+    if (isNew) {
       filtered = filtered.filter((product) => product.isNew);
     }
 
     setFilteredProducts(filtered);
     setCurrentPage(1);
     scrollToProductsSection();
+  };
+
+  const clearFilters = () => {
+    setFilteredProducts(allProducts);
+    setCurrentPage(1);
   };
 
   const handleSelectProduct = (sku: string) => {
@@ -119,53 +125,61 @@ function CardShop() {
     <section>
       <Filter
         applyFilters={applyFilters}
+        clearFilters={clearFilters}
+        filtersVisible={filtersVisible}
         currentPage={currentPage}
         totalProducts={filteredProducts.length}
         itemsPerPage={itemsPerPage}
       />
       <div className="py-20 max-sm:px-4" id="products-section">
-        <div className="text-start font-poppins max-w-screen-xl mx-auto">
-          <div className="mt-12 grid gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
-            {paginatedProducts.map((product) => (
-              <CardItem
-                key={product.sku}
-                product={product}
-                onSelectProduct={handleSelectProduct}
-              />
-            ))}
+        {filteredProducts.length === 0 ? (
+          <div className="flex items-center justify-center">
+            <p className="text-gray text-xl">There's nothing to see here :(</p>
           </div>
-          <div className="flex justify-center mt-8">
-            {currentPage >= 2 && (
-              <button
-                onClick={handlePreviousPage}
-                className="px-4 py-2 mx-1 rounded bg-filter hover:bg-primary hover:text-white transition duration-300"
-              >
-                Previous
-              </button>
-            )}
-            {getPageNumbers().map((page) => (
-              <button
-                key={page}
-                onClick={() => handlePageChange(page)}
-                className={`px-4 py-2 mx-1 rounded ${
-                  currentPage === page
-                    ? "bg-primary text-white"
-                    : "hover:bg-primary hover:text-white bg-filter"
-                }`}
-              >
-                {page}
-              </button>
-            ))}
-            {currentPage !== totalPages && (
-              <button
-                onClick={handleNextPage}
-                className="px-4 py-2 mx-1 rounded hover:bg-primary hover:text-white bg-filter transition duration-300"
-              >
-                Next
-              </button>
-            )}
+        ) : (
+          <div className="text-start font-poppins max-w-screen-xl mx-auto">
+            <div className="mt-12 flex flex-wrap justify-center gap-8">
+              {paginatedProducts.map((product) => (
+                <CardItem
+                  key={product.sku}
+                  product={product}
+                  onSelectProduct={handleSelectProduct}
+                />
+              ))}
+            </div>
+            <div className="flex justify-center mt-8">
+              {currentPage >= 2 && (
+                <button
+                  onClick={handlePreviousPage}
+                  className="px-4 py-2 mx-1 rounded bg-filter hover:bg-primary hover:text-white transition duration-300"
+                >
+                  Previous
+                </button>
+              )}
+              {getPageNumbers().map((page) => (
+                <button
+                  key={page}
+                  onClick={() => handlePageChange(page)}
+                  className={`px-4 py-2 mx-1 rounded ${
+                    currentPage === page
+                      ? "bg-primary text-white"
+                      : "hover:bg-primary hover:text-white bg-filter"
+                  }`}
+                >
+                  {page}
+                </button>
+              ))}
+              {currentPage !== totalPages && (
+                <button
+                  onClick={handleNextPage}
+                  className="px-4 py-2 mx-1 rounded hover:bg-primary hover:text-white bg-filter transition duration-300"
+                >
+                  Next
+                </button>
+              )}
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </section>
   );
--- src/components/Shop/Filter.tsx
+++ src/components/Shop/Filter.tsx
@@ -6,23 +6,26 @@ interface FilterProps {
     sortBy: string;
     isNew: boolean;
   }) => void;
+  clearFilters: () => void;
   currentPage: number;
   totalProducts: number;
   itemsPerPage: number;
+  filtersVisible: boolean;
 }
 
 const Filter = ({
   applyFilters,
+  clearFilters,
   currentPage,
   totalProducts,
   itemsPerPage,
 }: FilterProps) => {
-  const [isPopupOpen, setIsPopupOpen] = useState<boolean>(false);
+  const [filtersVisible, setFiltersVisible] = useState<boolean>(false);
   const [tempCategory, setTempCategory] = useState<string>("");
   const [tempSortBy, setTempSortBy] = useState<string>("");
 
-  const togglePopup = () => {
-    setIsPopupOpen(!isPopupOpen);
+  const toggleFiltersVisible = () => {
+    setFiltersVisible(!filtersVisible);
   };
 
   const handleCategoryChange = (event: ChangeEvent<HTMLSelectElement>) => {
@@ -40,99 +43,100 @@ const Filter = ({
       sortBy: isNew ? "" : tempSortBy,
       isNew,
     });
+  };
+
+  const handleClearButtonClick = () => {
+    clearFilters();
     setTempCategory("");
     setTempSortBy("");
-    togglePopup();
   };
 
   const start = (currentPage - 1) * itemsPerPage + 1;
   const end = Math.min(currentPage * itemsPerPage, totalProducts);
 
   return (
-    <div className="flex flex-col sm:flex-row items-center bg-filter p-4 pl-16 pr-16 font-poppins relative">
-      <div className="flex items-center mb-4 sm:mb-0">
-        <button onClick={togglePopup} className="focus:outline-none">
+    <div className="font-poppins">
+      <div className="flex flex-col sm:flex-row items-center bg-filter p-4 pl-16 pr-16 relative">
+        <div className="flex items-center mb-4 sm:mb-0">
+          <button onClick={toggleFiltersVisible} className="focus:outline-none">
+            <img src="/filtro.png" alt="Filtro" className="w-6 h-6" />
+          </button>
+          <span onClick={toggleFiltersVisible} className="ml-2 cursor-pointer">
+            Filter
+          </span>
           <img
-            src="https://desafio3furniro.s3.us-east-2.amazonaws.com/shop/filtro.png"
-            alt="Filtro"
-            className="w-6 h-6"
+            src="/bolinha.png"
+            alt="bolinhas"
+            className="cursor-pointer w-6 h-6 ml-4"
           />
-        </button>
-        <span onClick={togglePopup} className="ml-2 cursor-pointer">
-          Filter
-        </span>
-        <img
-          src="https://desafio3furniro.s3.us-east-2.amazonaws.com/shop/bolinhas.png"
-          alt="bolinhas"
-          className="cursor-pointer w-6 h-6 ml-4"
-        />
-        <img
-          src="https://desafio3furniro.s3.us-east-2.amazonaws.com/shop/lista.png"
-          alt="page"
-          className="cursor-pointer w-6 h-6 ml-4"
-        />
-        <div className="bg-black h-6 w-0.5 ml-8 mr-4"></div>
-        <span className="ml-4 text-sm">
-          Showing {start}-{end} of {totalProducts} results
-        </span>
-      </div>
-      <div className="flex items-center lg:ml-auto">
-        <label className="mr-2 text-sm">Show</label>
-        <div className="mr-4 py-2 px-4 text-gray bg-white text-sm">
-          {Math.min(itemsPerPage, totalProducts)}
+          <img
+            src="/view-list.png"
+            alt="page"
+            className="cursor-pointer w-6 h-6 ml-4"
+          />
+          <div className="bg-black h-6 w-0.5 ml-8 mr-4"></div>
+          <span className="ml-4 text-sm">
+            Showing {start}-{end} of {totalProducts} results
+          </span>
         </div>
-        <label className="mr-2 text-sm">Sort by</label>
-        <div className="bg-white text-gray py-2 pl-4 pr-10 text-sm">
-          Default
+        <div className="flex items-center lg:ml-auto">
+          <label className="mr-2 text-sm">Show</label>
+          <div className="mr-4 py-2 px-4 text-gray bg-white text-sm">
+            {Math.min(itemsPerPage, totalProducts)}
+          </div>
+          <label className="mr-2 text-sm">Sort by</label>
+          <div className="bg-white text-gray py-2 pl-4 pr-10 text-sm">
+            Default
+          </div>
         </div>
       </div>
+      {filtersVisible && (
+        <div className="flex flex-wrap items-center justify-center bg-filter p-4 pl-16 pr-16">
+          <select
+            onChange={handleCategoryChange}
+            className="p-2 border border-gray rounded hover:scale-105"
+            value={tempCategory}
+            data-testid="category-select"
+          >
+            <option value="" disabled>
+              Category
+            </option>
+            <option value="Sofa">Sofa</option>
+            <option value="Chair">Chair</option>
+            <option value="Table">Table</option>
+            <option value="Pot">Pot</option>
+            <option value="Mug">Mug</option>
+            <option value="Bed">Bed</option>
+            <option value="Lamp">Lamp</option>
+          </select>
 
-      {isPopupOpen && (
-        <div
-          className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50"
-          role="dialog"
-        >
-          <div className="p-6 rounded-lg relative bg-pink">
+          <select
+            onChange={handleSortByChange}
+            className="p-2 border border-gray rounded hover:scale-105 ml-4"
+            value={tempSortBy}
+            data-testid="sort-select"
+          >
+            <option value="" disabled>
+              Sort By
+            </option>
+            <option value="alphabetical">Alphabetical</option>
+            <option value="price">Price</option>
+            <option value="new">New Arrivals</option>
+          </select>
+
+          <div className="flex space-x-4 mt-4 sm:mt-0 ml-4">
             <button
-              onClick={togglePopup}
-              className="absolute top-2 right-2 text-2xl"
+              onClick={handleApplyButtonClick}
+              className="bg-primary text-white py-2 px-4 rounded hover:bg-opacity-90 transition duration-300"
             >
-              &times;
+              Apply Filters
             </button>
-            <div className="flex flex-col space-y-4">
-              <select
-                onChange={handleCategoryChange}
-                className="p-2 border border-gray rounded hover:scale-105"
-                value={tempCategory}
-                data-testid="category-select"
-              >
-                <option value="">Category</option>
-                <option value="Sofa">Sofa</option>
-                <option value="Chair">Chair</option>
-                <option value="Table">Table</option>
-                <option value="Pot">Pot</option>
-                <option value="Mug">Mug</option>
-                <option value="Bed">Bed</option>
-                <option value="Lamp">Lamp</option>
-              </select>
-              <select
-                onChange={handleSortByChange}
-                className="p-2 border border-gray rounded hover:scale-105"
-                value={tempSortBy}
-                data-testid="sort-select"
-              >
-                <option value="">Sort by</option>
-                <option value="alphabetical">Alphabetical</option>
-                <option value="price">Price</option>
-                <option value="new">New</option>
-              </select>
-              <button
-                onClick={handleApplyButtonClick}
-                className="p-2 border border-gray rounded bg-light-gray hover:scale-105"
-              >
-                Apply
-              </button>
-            </div>
+            <button
+              onClick={handleClearButtonClick}
+              className="bg-gray text-white py-2 px-4 rounded hover:bg-opacity-90 transition duration-300"
+            >
+              Clear Filters
+            </button>
           </div>
         </div>
       )}
