49e55427066f95c4ad6886dc6a7bed1b62ec4a0d
Biazanchin <beatrizzanquimmessias@gmail.com>
2024-08-27 17:11:05 -0300

    chore: delete configuration

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-    preset: 'ts-jest',
-    testEnvironment: 'jsdom',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    transform: {
-        '^.+\\.tsx?$': 'ts-jest',
-    },
-    testMatch: ['**/?(*.)+(spec|test).ts?(x)',],
-};
